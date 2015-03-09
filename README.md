0f75fcd7de679caae2b9f138c0b81e4c8cfc5930
Michael Nelson <michaelnelson07@gmail.com>
2015-03-09 15:30:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+## MLS Standings Scraper
+
+This is a simple web scraper written in Python to retrieve the standings from [mlssoccer.com][1].
+
+The idea stemmed from the desire to quickly interpret the standings in to a Markdown friendly format for the [FCD subreddit][2] sidebar.
+
+## Contributing
+Currently the script is setup to only parse the Western Table. Adding support for the Eastern table is trivial, and eventually I'd like to extend this to allow the user to pick which data points they desire.
+
+If you would like to contribute a new feature or bug fix:
+
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am 'Add some feature'`)
+3. Push to the branch (`git push origin my-new-feature`)
+4. Create a new Pull Request
+
+## License
+[Apache License 2.0][3]
+
+[1]: http://www.mlssoccer.com/
+[2]: http://www.reddit.com/r/fcdallas/
+[3]: http://www.apache.org/licenses/LICENSE-2.0
---
+++ mls.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+import requests
+import bs4
+
+# Setup data for club information
+all_clubs = {
+	# MLS Club Name: Abbreviation, Subreddit
+	'Colorado Rapids': ['COL', '/r/rapids'],
+	'FC Dallas': ['DAL', '/r/fcdallas'],
+	'Houston Dynamo': ['HOU', '/r/dynamo'],
+	'LA Galaxy': ['LA', '/r/lagalaxy'],
+	'Portland Timbers': ['POR', '/r/timbers'],
+	'Real Salt Lake': ['RSL', '/r/realsaltlake'],
+	'San Jose Earthquakes': ['SJ', '/r/sjearthquakes'],
+	'Seattle Sounders FC': ['SEA', '/r/soundersfc'],
+	'Sporting Kansas City': ['SKC', '/r/sportingkc'],
+	'Vancouver Whitecaps FC': ['VAN', '/r/whitecapsfc']
+}
+
+# Setup our variables for URLs and data to parse
+standings_url = requests.get('http://www.mlssoccer.com/standings')
+standing_soup = bs4.BeautifulSoup(standings_url.text)
+
+# Western Conference is the 2nd table
+standings = standing_soup.select('.stats-table')[1]
+standings_data = standings.find('tbody').find_all('tr')
+
+def strip_text(text):
+	return text.get_text().strip();
+
+def link_club(_club):
+	abbr = all_clubs[_club][0]
+	sub = all_clubs[_club][1]
+	club = '[%s](%s)' % (abbr, sub)
+	return club
+
+def format_club(_club):
+	club = strip_text(_club)
+	club = link_club(club)
+	return club
+
+# Print the standings to stdout
+for row in standings_data:
+	cells = row.find_all('td')
+	rank = strip_text(cells[0])
+	club = format_club(cells[1].find('a')) # find 'a' to ignore playoff indicator
+	points = '**%s**' % strip_text(cells[2])
+	gp = strip_text(cells[3])
+	gd = strip_text(cells[10])
+	gf = strip_text(cells[8])
+	print '%s|%s|%s|%s|%s|%s' % (rank, club, points, gp, gd, gf)
